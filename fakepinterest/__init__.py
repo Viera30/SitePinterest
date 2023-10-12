0c0b829997f8a7a8358e13bb40c79d2c4283ad9d
Matheus Vieira <matheus.vieirasilva@hotmail.com>
2023-10-12 12:27:45 -0300

    primeiro commit

---
+++ fakepinterest/__init__.py
@@ -0,0 +1,22 @@
+#inicializar
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+
+
+
+
+
+
+app = Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SECRET_KEY"] = "5edace8fdc83b69d050b20fb890bec722dc5b8254ffd5abb3cfe5b05e4b838dd"
+app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
+
+database = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "homepage"
+
+from fakepinterest import routes
