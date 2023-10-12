29db32ff7a83194969b64231d36a4b318b41c3f0
Matheus Vieira <matheus.vieirasilva@hotmail.com>
2023-10-12 15:27:48 -0300

    save  psycopg2

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -11,7 +11,7 @@ import os
 
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] =os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_DATABASE_URI"] ="postgresql://banco_web_pinterest_user:3sA3BGXmYubrD5TnMNwYBwrpROqG2uJa@dpg-ckk3d0ekpues73d9ea70-a.oregon-postgres.render.com/banco_web_pinterest"
 app.config["SECRET_KEY"] = "5edace8fdc83b69d050b20fb890bec722dc5b8254ffd5abb3cfe5b05e4b838dd"
 app.config["UPLOAD_FOLDER"] = "static/fotos_posts"
 
